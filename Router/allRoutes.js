212b77fa19b61588a00427ef062053e4e243ddd3
benbadj <benbadj0033@gmail.com>
2026-06-19 19:08:26 +0200

    ajout auth et fin html

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,9 +5,10 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/main", "menu", "/pages/main.html"),
     new Route("/contact", "contact", "/page/contact.html"),
-    new Route("/auth", "auth", "/pages/auth.html"),
-    new Route("/signup", "signup", "/pages/signup.html"),
-    new Route("/reservation", "reservation", "/pages/reservation.html"),
+    new Route("/auth", "connexion", "/pages/auth/signin.html"),
+    new Route("/signup", "signup", "/pages/auth/signup.html"),
+    new Route("/allsreservations", "allresrvation", "/pages/reservations/allsreservations.html"),
+    new Route("/reservation", "reservation", "/pages/reservations/reserver.html"),
     new Route("/404", "404", "/pages/404.html"),
 ];
 
--- Router/router.js
+++ Router/router.js
@@ -1,57 +1,47 @@
-import Route from "./Route.js";
+//1. on attend que le html initial soit soit completement chargé
+document.addEventListener("DOMContentLoaded", function() { 
+  const appContainer = document.getElementById("main-page");
+  const navLinks = document.querySelectorAll(".nav-link");
+  //fonction pour charger le contenu d'une page de manière assynchrome
+  async function loadPage(url) {
+    try {
+      console.log("tentative de chargement de :", url)
+      //on recupere le fichier html demandé (ex: /page/main.html)
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error("Page non trouvée")
+      }
+      const htmlContent = await response.text();
 
-import { allRoutes, websiteName } from "./allRoutes.js";
-// Création d'une route pour la page 404 (page introuvable)
-const route404 = new Route("404", "Page introuvable", "/pages/404.html");
-// Fonction pour récupérer la route correspondant à une URL donnée
-const getRouteByUrl = (url) => {
-  let currentRoute = null;
-  // Parcours de toutes les routes pour trouver la correspondance
-  allRoutes.forEach((element) => {
-    if (element.url == url) {
-      currentRoute = element;
+      //on injecte le contenu récupéré dans notre container principal
+      appContainer.innerHTML = htmlContent;
+    } catch (error) {
+      console.error(error);
+      appContainer.innerHTML = '<h2>Erreur 404</h2><p>Désolé, impossible de chargé la page.</p>';
     }
+    
+  }
+  //2.on écoute tous les clics sur tous les liens du menu
+    document.addEventListener("click", function(event) {
+      //on cherche si l'élément cliqué ou son parent est un lien avec la class .nav-link
+      const link = even.target.closest(".nav-link");
+      //si on a bien cliqué sur le lien de menu
+      if (link) {
+          even.preventDefault();
+      //on recupere la cible du lien (ex:pafe/main.html)
+      let pageUrl = link.getAttribute("href");
+      //si c'est le lien home"/", on le redirige vers notre page d'accueil réelle
+      if (pageUrl === "/" || pageUrl === ""){
+        loadPage("/home.html");
+      } else {
+        //si le lien commence par un "/", on l'enleve pour repartir propreement de la racine du navigateur
+        if (pageUrl.startsWith("/")) {
+            pageUrl = pageUrl.substring(1); //devient "pages/main.html"
+        }
+        loadPage(pageUrl);
+      }
+    };
   });
-  // Si aucune correspondance n'est trouvée, on retourne la route 404
-  if (currentRoute != null) {
-    return currentRoute;
-  } else {
-    return route404;
-  }
-};
-// Fonction pour charger le contenu de la page
-const LoadContentPage = async () => {
-  const path = window.location.pathname;
-  // Récupération de l'URL actuelle
-  const actualRoute = getRouteByUrl(path);
-  // Récupération du contenu HTML de la route
-  const html = await fetch(actualRoute.pathHtml).then((data) => data.text());
-  // Ajout du contenu HTML à l'élément avec l'ID "main-page"
-  document.getElementById("main-page").innerHTML = html;
-  // Ajout du contenu JavaScript
-  if (actualRoute.pathJS != "") {
-    // Création d'une balise script
-    var scriptTag = document.createElement("script");
-    scriptTag.setAttribute("type", "text/javascript");
-    scriptTag.setAttribute("src", actualRoute.pathJS);
-    // Ajout de la balise script au corps du document
-    document.querySelector("body").appendChild(scriptTag);
-  }
-  // Changement du titre de la page
-  document.title = actualRoute.title + " - " + websiteName;
-};
-// Fonction pour gérer les événements de routage (clic sur les liens)
-const routeEvent = (event) => {
-  event = event || window.event;
-  event.preventDefault();
-  // Mise à jour de l'URL dans l'historique du navigateur
-  window.history.pushState({}, "", event.target.href);
-  // Chargement du contenu de la nouvelle page
-  LoadContentPage();
-};
-// Gestion de l'événement de retour en arrière dans l'historique du navigateur
-window.onpopstate = LoadContentPage;
-// Assignation de la fonction routeEvent à la propriété route de la fenêtre
-window.route = routeEvent;
-// Chargement du contenu de la page au chargement initial
-LoadContentPage();
+  //3. charger la page d'accueil par defaut au premier démarrage
+  loadPage("pages/home.html");
+});
