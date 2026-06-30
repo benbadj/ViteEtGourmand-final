2d7dd0e76d69d1ee089a1da1fd610aec28123e49
benbadj <benbadj0033@gmail.com>
2026-06-30 14:49:47 +0200

    authorize routes

--- Router/Route.js
+++ Router/Route.js
@@ -1,9 +1,19 @@
 
 export default class Route {
-    constructor(url, title, pathHtml, pathJS = "") {
+    constructor(url, title, pathHtml, authorize, pathJS = "") {
       this.url = url;
       this.title = title;
       this.pathHtml = pathHtml;
       this.pathJS = pathJS;
+      this.authorize = authorize; // pour le controle de sécurité
     }
 }
+
+
+//on rajoute des roles d'autorisation, on en a 4 possibilité
+/*
+[] => tout le monde peut y acceder
+ ["disconnected"] => réservé aux utilisateurs déconnecté
+ ["client"] => réserver aux utilisateurs avec le role client
+ ["admin"] => réserver aux utilisateurs avec le role admin
+ ["admin", "client"] => réserver aux utilisateurs avec le role client ou admin */
--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,16 +2,16 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/main", "menu", "/pages/main.html"),
-    new Route("/contact", "contact", "/pages/contact.html"),
-    new Route("/moncompte", "moncompte", "/pages/moncompte.html"),
-    new Route("/signin", "connexion", "/pages/auth/signin.html", "/js/auth/signin.js"),
-    new Route("/signup", "Inscription", "/pages/auth/signup.html", "/js/auth/signup.js"),
-    new Route("/editpassword", "editpassword", "/pages/auth/editpassword.html"),
-    new Route("/allsreservations", "allresrvations", "/pages/reservations/allsreservations.html"),
-    new Route("/reserver", "reserver", "/pages/reservations/reserver.html"),
-    new Route("/404", "404", "/pages/404.html"),
+    new Route("/", "Accueil", "/pages/home.html", []),
+    new Route("/main", "menu", "/pages/main.html", [], "/js/main.js"),
+    new Route("/contact", "contact", "/pages/contact.html", ["disconnected"], "/js/contact.js"),
+    new Route("/moncompte", "moncompte", "/pages/moncompte.html", ["client", "admin"]),
+    new Route("/signin", "connexion", "/pages/auth/signin.html", ["disconnected"], "/js/auth/signin.js"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html", ["disconnected"], "/js/auth/signup.js"),
+    new Route("/editpassword", "editpassword", "/pages/auth/editpassword.html", ["client", "admin"]),
+    new Route("/allsreservations", "allresrvations", "/pages/reservations/allsreservations.html", ["client"]),
+    new Route("/reserver", "reserver", "/pages/reservations/reserver.html", ["client"]),
+    new Route("/404", "404", "/pages/404.html", []),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
